28c2cb7d5ca007c9be8a0366e52622a5facb3ff9
Eihaab-cmyk <135423812+Eihaab-cmyk@users.noreply.github.com>
2023-10-08 22:45:40 +0500

    Add files via upload

---
+++ CreateContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CreateContainer = () => {
+  return (
+    <div>
+      ggggg
+    </div>
+  )
+}
+
+export default CreateContainer
+
--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-import React from "react";
-import ReactDOM from 'react-dom'
-import './index.css'
-
-import { BrowserRouter as Router } from "react-router-dom";
-import App from './App';
-import reducer from "./context/reducer";
-import { StateProvider } from "./context/StateProvider";
-import { initialState } from "./context/initialState";
-
-ReactDOM.render(
-<Router>
-    <StateProvider initialState={initialState} reducer={reducer}>
-        <App />
-    </StateProvider>
-</Router>, 
-document.getElementById("root")
-);
+export {default as Header} from './Header'
+export {default as MainContainer} from './MainContainer'
+export {default as CreateContainer} from './CreateContainer'
