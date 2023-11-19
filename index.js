de83932bf12a4a2e0f2d1d348f5bc647e8a71a52
Eihaab-cmyk <135423812+Eihaab-cmyk@users.noreply.github.com>
2023-11-19 22:57:44 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,109 +1,20 @@
-import axios from "axios";
+import {combineReducers} from 'redux';
+import userReducer from './userReducer';
+import alertReducer from './alertReducer';
+import productReducer from './productReducer';
+import allUserReducer from './allUserReducer';
+import cartReducer from './cartReducer';
+import displayCartReducer from './displayCartReducer';
+import ordersReducer from './ordersReducer';
 
-export const baseURL = "http://localhost:5001/se-project-58135/us-central1/app";
+const myReducers = combineReducers({
+    user: userReducer,
+    alert: alertReducer,
+    products: productReducer,
+    allUsers: allUserReducer,
+    cart: cartReducer,
+    isCart: displayCartReducer,
+    orders: ordersReducer,
+});
 
-export const validateUserJWTToken = async (token) => {
-    try {
-        const res = await axios.get(`${baseURL}/api/users/jwtVerfication`, {
-            headers: {Authorization: "Bearer " + token},
-        });
-        return res.data.data;
-    } catch (err) {
-        return (null);
-    }
-};
-
-// add new product
-export const addNewProduct = async (data) => {
-    try{
-        const res = await axios.post(`${baseURL}/api/products/create`, {...data});
-        return res.data.data;
-    } catch (err) {
-        return null;
-    }
-};
-
-// get all the products
-export const getAllProducts = async () => {
-    try{
-        const res = await axios.get(`${baseURL}/api/products/all`);
-        return res.data.data;
-    } catch (err) {
-        return null;
-    }
-};
-
-// delete a product
-export const deleteAProduct = async (productId) => {
-    try{
-        const res = await axios.delete(`${baseURL}/api/products/delete/${productId}`);
-        return res.data.data;
-    } catch (err) {
-        return null;
-    }
-};
-
-export const getAllUsers = async () => {
-    try {
-        const res = await axios.get(`${baseURL}/api/users/all`);
-        console.log('Full API Response:', res);
-        console.log('Data from API:', res.data.data); // Log the data
-        return res.data.data;
-    } catch (err) {
-        console.error('Error fetching data:', err);
-        return null;
-    }
-};
-
-// add an item to cart
-export const addNewItemToCart = async (user_Id, data) => {
-    try {
-        const res = await axios.post(`${baseURL}/api/products/addToCart/${user_Id}`,{...data});
-        console.log('Full API Response:', res);
-        console.log('Data from API:', res.data.data); // Log the data
-        return res.data.data;
-    } catch (err) {
-        //console.error('Error fetching data:', err);
-        return null;
-    }
-};
-
-// get all cart items
-export const getAllCartItems = async (user_Id) => {
-    try {
-        const res = await axios.get(`${baseURL}/api/products/getCartItems/${user_Id}`);
-        console.log('Full API Response:', res);
-        console.log('Data from API:', res.data.data); // Log the data
-        return res.data.data;
-    } catch (err) {
-        //console.error('Error fetching data:', err);
-        return null;
-    }
-};
-
-// cart increment
-export const increaseItemQuantity = async (user_Id, productId, type) => {
-    console.log(user_Id, productId, type);
-    try {
-        const res = await axios.post(`${baseURL}/api/products/updateCart/${user_Id}`, null, {params: {productId: productId, type: type}});
-        console.log('Full API Response:', res);
-        console.log('Data from API:', res.data.data); // Log the data
-        return res.data.data;
-    } catch (err) {
-        //console.error('Error fetching data:', err);
-        return null;
-    }
-};
-
-// get all orders
-export const getAllorder = async () => {
-    try {
-        const res = await axios.get(`${baseURL}/api/products/orders`);
-        console.log('Full API Response:', res);
-        console.log('Data from API:', res.data.data); // Log the data
-        return res.data.data;
-    } catch (err) {
-        //console.error('Error fetching data:', err);
-        return null;
-    }
-};
+export default myReducers;
