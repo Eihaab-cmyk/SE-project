6564d7649a13d5a5e97dc5c316acd8bdee966439
Eihaab-cmyk <135423812+Eihaab-cmyk@users.noreply.github.com>
2023-11-19 22:56:09 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,109 @@
-export {default as LoginInput} from "./LoginInput";
-export {default as MainLoader} from "./MainLoader";
-export {default as Alert} from "./Alert";
-export {default as Header} from "./Header";
+import axios from "axios";
 
-export {default as DBLeftSection} from "./DBLeftSection";
-export {default as DBRightSection} from "./DBRightSection";
-export {default as DBHeader} from "./DBHeader";
+export const baseURL = "http://localhost:5001/se-project-58135/us-central1/app";
 
-export {default as DBHome} from "./DBHome";
-export {default as DBOrders} from "./DBOrders";
-export {default as DBItems} from "./DBItems";
-export {default as DBNewItem} from "./DBNewItem";
-export {default as DBUsers} from "./DBUsers";
+export const validateUserJWTToken = async (token) => {
+    try {
+        const res = await axios.get(`${baseURL}/api/users/jwtVerfication`, {
+            headers: {Authorization: "Bearer " + token},
+        });
+        return res.data.data;
+    } catch (err) {
+        return (null);
+    }
+};
 
-export {default as DataTable} from "./DataTable";
+// add new product
+export const addNewProduct = async (data) => {
+    try{
+        const res = await axios.post(`${baseURL}/api/products/create`, {...data});
+        return res.data.data;
+    } catch (err) {
+        return null;
+    }
+};
 
-export {default as Spinner} from "./Spinner";
-export {default as Home} from "./Home";
-export {default as HomeSlider} from "./HomeSlider";
-export {default as Slider} from "./Slider";
-export {default as SliderCard} from "./SliderCard";
-export {default as FilterSection} from "./FilterSection";
-export {default as Cart} from "./Cart";
-export {default as CheckOutSuccess} from "./CheckOutSuccess";
-export {default as OrderData} from "./OrderData";
+// get all the products
+export const getAllProducts = async () => {
+    try{
+        const res = await axios.get(`${baseURL}/api/products/all`);
+        return res.data.data;
+    } catch (err) {
+        return null;
+    }
+};
+
+// delete a product
+export const deleteAProduct = async (productId) => {
+    try{
+        const res = await axios.delete(`${baseURL}/api/products/delete/${productId}`);
+        return res.data.data;
+    } catch (err) {
+        return null;
+    }
+};
+
+export const getAllUsers = async () => {
+    try {
+        const res = await axios.get(`${baseURL}/api/users/all`);
+        console.log('Full API Response:', res);
+        console.log('Data from API:', res.data.data); // Log the data
+        return res.data.data;
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        return null;
+    }
+};
+
+// add an item to cart
+export const addNewItemToCart = async (user_Id, data) => {
+    try {
+        const res = await axios.post(`${baseURL}/api/products/addToCart/${user_Id}`,{...data});
+        console.log('Full API Response:', res);
+        console.log('Data from API:', res.data.data); // Log the data
+        return res.data.data;
+    } catch (err) {
+        //console.error('Error fetching data:', err);
+        return null;
+    }
+};
+
+// get all cart items
+export const getAllCartItems = async (user_Id) => {
+    try {
+        const res = await axios.get(`${baseURL}/api/products/getCartItems/${user_Id}`);
+        console.log('Full API Response:', res);
+        console.log('Data from API:', res.data.data); // Log the data
+        return res.data.data;
+    } catch (err) {
+        //console.error('Error fetching data:', err);
+        return null;
+    }
+};
+
+// cart increment
+export const increaseItemQuantity = async (user_Id, productId, type) => {
+    console.log(user_Id, productId, type);
+    try {
+        const res = await axios.post(`${baseURL}/api/products/updateCart/${user_Id}`, null, {params: {productId: productId, type: type}});
+        console.log('Full API Response:', res);
+        console.log('Data from API:', res.data.data); // Log the data
+        return res.data.data;
+    } catch (err) {
+        //console.error('Error fetching data:', err);
+        return null;
+    }
+};
+
+// get all orders
+export const getAllorder = async () => {
+    try {
+        const res = await axios.get(`${baseURL}/api/products/orders`);
+        console.log('Full API Response:', res);
+        console.log('Data from API:', res.data.data); // Log the data
+        return res.data.data;
+    } catch (err) {
+        //console.error('Error fetching data:', err);
+        return null;
+    }
+};
